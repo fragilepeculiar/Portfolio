d127a8a6929af21265da989dae0bb072a42c0a0b
desertsfan <desertsfan@gmail.com>
2017-07-18 22:10:17 +0800

    123

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,33 @@
-/**
- * Created by deserts on 2017/7/17.
- */
+//
+// const gulp = require('gulp'),
+//     extender = require('gulp-html-extend');
+//
+// const input_path = "./src/",
+//     output_path = "./dist/";
+//
+// const html_path = input_path + "";
+//
+// var htmls = ["portfolio"],
+//     html_list = [];
+//
+// for (index in htmls) {
+//     html_list[index] = html_path + htmls[index] + ".html";
+// }
+//
+// console.log(html_list);
+//
+// gulp.task('extend', function () {
+//     gulp.src(html_list)
+//         .pipe(extender({annotations: true, verbose: false}))
+//         .pipe(gulp.dest(output_path));
+// });
+//
+//
+// gulp.task('watch', function () {
+//     gulp.watch(html_path + '/*.html', ['extend']);
+// });
+//
+// gulp.task('default', function () {
+//     gulp.run('extend');
+//     // gulp.run('watch');   # 暂时无需监听，注释掉
+// });
