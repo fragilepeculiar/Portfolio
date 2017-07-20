dba74a21cbd4241d6af37d5ceaca529c7e66ac27
desertfan <desertsfan@gmail.com>
2017-07-20 15:20:09 +0800

    0720afternoon

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var htmlminify = require("gulp-html-minify");
 var imagemin = require("gulp-imagemin")
 var minifyCSS = require('gulp-minify-css')
+var uglify = require('gulp-uglify')
 
 // 压缩 css 文件
 // 在命令行使用 gulp css 启动此任务
@@ -15,27 +16,7 @@ gulp.task('css', function () {
         .pipe(gulp.dest('dist/CSS'))
 })
 
-// 在命令行使用 gulp auto 启动此任务
-gulp.task('auto', function () {
-    // 监听文件修改，当文件被修改则执行 css 任务
-    gulp.watch('CSS/*.css', ['css'])
-    gulp.watch('img/*.*)', ['img'])
-    gulp.watch('*.html',[])
-});
-
-// 使用 gulp.task('default') 定义默认任务
-// 在命令行使用 gulp 启动css等任务
-gulp.task('default', ['css', 'auto', 'build-html', 'img'])
-
-
-gulp.task('build-html' , function(){
-    return gulp.src("*.html")
-        .pipe(htmlminify())
-        .pipe(gulp.dest("dist"))
-});
-
 // 压缩图片任务
-// 在命令行输入 gulp images 启动此任务
 gulp.task('img', function () {
     // 1. 找到图片
     gulp.src('img/*.*')
@@ -46,5 +27,41 @@ gulp.task('img', function () {
         // 3. 另存图片
         .pipe(gulp.dest('dist/img'))
 });
+
+// 压缩html任务
+gulp.task('build-html' , function(){
+    return gulp.src("*.html")
+        .pipe(htmlminify())
+        .pipe(gulp.dest("dist"))
+});
+
+
+// 压缩 js 文件
+gulp.task('script', function() {
+    // 1. 找到文件
+    gulp.src('JS/*.js')
+    // 2. 压缩文件
+        .pipe(uglify())
+        // 3. 另存压缩后的文件
+        .pipe(gulp.dest('dist/JS'))
+})
+
+
+// 在命令行使用 gulp auto 启动此任务
+gulp.task('auto', function () {
+    // 监听文件修改，当文件被修改则执行 css 任务
+    gulp.watch('CSS/*.css', ['css'])
+    gulp.watch('img/*.*)', ['img'])
+    gulp.watch('*.html',['build-html'])
+    gulp.watch('JS/*.js', ['script'])
+});
+
+// 在命令行使用 gulp 启动css等任务
+gulp.task('default', ['css', 'auto', 'build-html', 'img', 'script'])
+
+
+
+
+
 
 
