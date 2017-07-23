2abc2d4bd6d02931a7d7880944edf3ff3542fe8e
desertsfan <desertsfan@gmail.com>
2017-07-23 17:54:42 +0800

    dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 // 获取 gulp
 var gulp = require('gulp');
 var htmlminify = require("gulp-html-minify");
-var imagemin = require("gulp-imagemin")
-var minifyCSS = require('gulp-minify-css')
-var uglify = require('gulp-uglify')
+var imagemin = require("gulp-imagemin");
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
 // 压缩 css 文件
 // 在命令行使用 gulp css 启动此任务
 gulp.task('css', function () {
     // 1. 找到文件
-    gulp.src('css/*.css')
+    gulp.src('src/css/*.css')
     // 2. 压缩文件
         .pipe(minifyCSS())
         // 3. 另存为压缩文件
@@ -19,7 +19,7 @@ gulp.task('css', function () {
 // 压缩图片任务
 gulp.task('img', function () {
     // 1. 找到图片
-    gulp.src('img/*.*')
+    gulp.src('src/img/*.*')
     // 2. 压缩图片
         .pipe(imagemin({
             progressive: true
@@ -30,16 +30,16 @@ gulp.task('img', function () {
 
 // 压缩html任务
 gulp.task('build-html' , function(){
-    return gulp.src("*.html")
+    return gulp.src("src/*.html")
         .pipe(htmlminify())
-        .pipe(gulp.dest("dist"))
+        .pipe(gulp.dest("dist/"))
 });
 
 
 // 压缩 js 文件
 gulp.task('script', function() {
     // 1. 找到文件
-    gulp.src('js/*.js')
+    gulp.src('src/js/*.js')
     // 2. 压缩文件
         .pipe(uglify())
         // 3. 另存压缩后的文件
@@ -50,15 +50,14 @@ gulp.task('script', function() {
 // 在命令行使用 gulp auto 启动此任务
 gulp.task('auto', function () {
     // 监听文件修改，当文件被修改则执行 css 任务
-    gulp.watch('css/*.css', ['css'])
-    gulp.watch('img/*.*)', ['img'])
-    gulp.watch('*.html',['build-html'])
-    gulp.watch('js/*.js', ['script'])
+    gulp.watch('src/css/*.css', ['css'])
+    gulp.watch('src/img/*.*)', ['img'])
+    gulp.watch('src/*.html',['build-html'])
+    gulp.watch('src/js/*.js', ['script'])
 });
 
 // 在命令行使用 gulp 启动css等任务
-gulp.task('default', ['css', 'auto', 'build-html', 'img', 'script'])
-
+gulp.task('default', ['css', 'img', 'build-html', 'script', 'auto'])
 
 
 
